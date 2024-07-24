d7c8f375b0670e1d6dcab8323e1b3340e99f4f4f
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-07-24 03:38:45 -0700

    rvu source function

--- data-raw/rvu_source.R
+++ data-raw/rvu_source.R
@@ -1,58 +1,112 @@
-url_to_scrape <- "https://www.cms.gov/medicare/payment/fee-schedules/physician/pfs-relative-value-files"
+download_rvu_info_tables <- function(year) {
 
-url_to_click <- rvest::read_html(url_to_scrape)
+  url_land <- "https://www.cms.gov/medicare/payment/fee-schedules/physician/pfs-relative-value-files"
 
-rvu_table <- rvest::html_table(url_to_click) |>
-  purrr::pluck(1) |>
-  dplyr::reframe(
-    year = as.integer(stringr::str_remove_all(Name, "\\D")),
-    name_file = stringr::str_remove_all(`File Name`, "\\n|File Name|\\s|.ZIP")
-  )
+  html_land <- rvest::read_html(url_land)
 
-rvu_urls <- url_to_click |>
-  rvest::html_elements("a") |>
-  rvest::html_attr("href") |>
-  collapse::funique() |>
-  stringr::str_subset(
-    stringr::regex(
-      paste(
-        "/medicare/payment/fee-schedules/physician/pfs-relative-value-files/",
-        "/medicare/medicare-fee-service-payment/physicianfeesched/pfs-relative-value-files/",
-        "/medicaremedicare-fee-service-paymentphysicianfeeschedpfs-relative-value-files/",
-        "/medicare/medicare-fee-for-service-payment/physicianfeesched/pfs-relative-value-files-items/",
-        sep = "|"
+  rvu_table <- rvest::html_table(html_land) |>
+    purrr::pluck(1) |>
+    dplyr::reframe(
+      year = as.integer(
+        stringr::str_remove_all(Name, "\\D")
+        ),
+      filename = stringr::str_remove_all(
+        `File Name`,
+        "\\n|File Name|\\s|.ZIP"
+        )
+    )
+
+  rvu_urls <- html_land |>
+    rvest::html_elements("a") |>
+    rvest::html_attr("href") |>
+    collapse::funique() |>
+    stringr::str_subset(
+      stringr::regex(
+        paste(
+          "/medicare/payment/fee-schedules/physician/pfs-relative-value-files/",
+          "/medicare/medicare-fee-service-payment/physicianfeesched/pfs-relative-value-files/",
+          "/medicaremedicare-fee-service-paymentphysicianfeeschedpfs-relative-value-files/",
+          "/medicare/medicare-fee-for-service-payment/physicianfeesched/pfs-relative-value-files-items/",
+          sep = "|"
+        )
       )
+    )
+
+  file_names_url <- strex::str_after_last(rvu_urls, "/")
+
+  link_table <- dplyr::bind_cols(
+    rvu_table,
+    dplyr::tibble(
+      filename_url = file_names_url,
+      url = paste0("https://www.cms.gov", rvu_urls)
     )
   )
 
-url_names <- rvu_urls |> strex::str_after_last("/")
+  urls_to_zips <- dplyr::filter(link_table, year == {{  year }})
 
-link_table <- dplyr::bind_cols(
-  rvu_table,
-  dplyr::tibble(
-    name_url = url_names,
-    url = paste0("https://www.cms.gov", rvu_urls)
-  )
-)
+  html_individual_pgs <- purrr::map(urls_to_zips$url, rvest::read_html)
 
+  zip_download_links <- purrr::map(
+    html_individual_pgs,
+    rvest::html_elements, css = "a") |>
+    purrr::map(rvest::html_attr, "href") |>
+    purrr::map(collapse::funique) |>
+    purrr::map(stringr::str_subset, ".zip") |>
+    purrr::map(\(x) stringr::str_c("https://www.cms.gov", x)) |>
+    unlist()
 
-urls_to_zips <- dplyr::filter(link_table, year == 2024)
+  zip_download_info <- purrr::map(html_individual_pgs, rvest::html_elements, css = ".field") |>
+    purrr::map(rvest::html_text2) |>
+    purrr::map(collapse::funique) |>
+    purrr::map(stringr::str_subset, "Dynamic List", TRUE) |>
+    purrr::map(stringr::str_subset, "File Name|Description|File Size|Downloads") |>
+    purrr::map(stringr::str_replace_all, "\\n", " ") |>
+    purrr::set_names(urls_to_zips$filename_url)
 
-download_pages <- purrr::map(urls_to_zips$url, rvest::read_html)
+  lookup <- purrr::set_names(1:12, month.name)
 
-purrr::map(download_pages, rvest::html_elements, css = ".field") |>
-  purrr::map(rvest::html_text2) |>
-  purrr::map(collapse::funique) |>
-  purrr::map(stringr::str_subset, "Dynamic List", TRUE)
+  zip_table <- dplyr::tibble(
+    zip_download_links,
+    zip_download_info
+  ) |>
+    tidyr::unnest(cols = zip_download_info) |>
+    dplyr::mutate(
+      col_name = dplyr::case_when(
+        stringr::str_detect(zip_download_info, "File Name") ~ "filename",
+        stringr::str_detect(zip_download_info, "Description") ~ "description",
+        stringr::str_detect(zip_download_info, "File Size") ~ "filesize",
+        stringr::str_detect(zip_download_info, "Downloads") ~ "downloads",
+        .default = NA_character_
+      ),
+      zip_download_info = stringr::str_remove_all(
+        zip_download_info,
+        "File Name |Description |File Size |Downloads ")) |>
+    tidyr::pivot_wider(
+      names_from = col_name,
+      values_from = zip_download_info) |>
+    dplyr::reframe(
+      file_name = filename,
+      last_updated = stringr::str_extract(downloads, "\\d{2}\\/\\d{2}\\/\\d{4}") |> anytime::anydate(),
+      date_effective = stringr::str_remove_all(description, "Medicare|Physician|Fee|Schedule|rates|effective|-|release") |>
+        stringr::str_squish() |>
+        stringr::str_extract("(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\\s+(\\d{1,2}\\,\\s+)?(\\d{4})"),
+      mon = stringr::str_extract(date_effective, "(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)"),
+      mon = lookup[mon],
+      day = stringr::str_extract(date_effective, "[0-9]{1,2}(?=,)") |> tidyr::replace_na("1") |> as.integer(),
+      year = stringr::str_extract(date_effective, "\\d{4}$") |> as.integer(),
+      date_effective = clock::date_build(year, mon, day),
+      zip_url = zip_download_links
+    ) |>
+    dplyr::select(
+      file_name,
+      last_updated,
+      date_effective,
+      zip_url
+    )
 
+  return(zip_table)
 
-download_zips <- purrr::map(download_pages, rvest::html_elements, css = "a") |>
-  purrr::map(rvest::html_attr, "href") |>
-  purrr::map(collapse::funique) |>
-  purrr::map(stringr::str_subset, ".zip") |>
-  purrr::map(\(x) stringr::str_c("https://www.cms.gov", x)) |>
-  unlist()
-
+}
 
 curl::multi_download(rvu_prefix)
 
