903de89967d1db7a68a9738661c7382759c4a669
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-07-25 18:08:27 -0700

    fix: get_source

--- R/get_rvu_files.R
+++ R/get_rvu_files.R
@@ -1,13 +1,37 @@
 #' Get RVU Files
 #'
-#' @returns NULL
+#' @param source rvu source (link, zip, file, pprvu, oppscap, gpci, locco, anes)
+#'
+#' @returns list of selected rvu source files
 #'
 #' @examples
-#' get_rvu_files()
+#' get_source("link")
+#'
+#' get_source("zip")
+#'
+#' get_source("file")
+#'
+#' get_source("pprvu")
+#'
+#' get_source("gpci")
 #'
 #' @autoglobal
 #'
 #' @export
-get_rvu_files <- function() {
-  get_pin("rvu_source_2024")
+get_source <- function(source = c("link", "zip", "file", "pprvu", "oppscap", "gpci", "locco", "anes")) {
+
+  source <- match.arg(source)
+
+  switch(
+    source,
+    link    = get_pin("rvu_source_2024")$link_table,
+    zip     = get_pin("rvu_source_2024")$zip_table,
+    file    = get_pin("rvu_source_2024")$zip_list,
+    pprvu   = get_pin("rvu_source_2024")$files$pprvu,
+    oppscap = get_pin("rvu_source_2024")$files$oppscap,
+    gpci    = get_pin("rvu_source_2024")$files$gpci,
+    locco   = get_pin("rvu_source_2024")$files$locco,
+    anes    = get_pin("rvu_source_2024")$files$anes
+  )
+
 }
